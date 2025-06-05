17dfefea393489c994a0e4532c805ef7c4f8eff3
Arihant200 <128326683+Arihant200@users.noreply.github.com>
2025-06-05 13:21:20 +0000

    Your message

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+console.log(2>1);
+console.log(null>0);
+console.log(null==0)
+console.log(null>=0) //converts numll to 0
+//comparison operator convert to number,Nan or 0
+
+//=== strict check
+console.log('2'==2)
+console.log('2'===2)//compare data type also 
---
+++ 01_basics/memory.js
@@ -0,0 +1,15 @@
+//stack(primitive),heap(non primitive)
+let myYoutubensame="arihant jain"
+let anothername=myYoutubensame
+anothername="cpp"
+console.log(anothername);
+console.log(myYoutubensame)
+
+let user1={
+    email:"user1@gmail.com",
+    upi:"ss1@ybl"
+}
+let user2=user1
+user2.email="arihant@gmial.com"
+console.log(user1.email)
+console.log(user2.email)
